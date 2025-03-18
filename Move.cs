a35395820d40b59fbbfa2c15bafb4525d2db560d
Trịnh Kiên <trinhkien1211@gmail.com>
2025-03-18 19:08:25 +0700

    add

---
+++ Move.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public int tocDo = 4;
+
+    public float traiPhai;
+
+    public bool isFacingRight = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        traiPhai = Input.GetAxisRaw("Horizontal"); // A = -1, 0, D = 1 
+        rb.velocity = new Vector2(tocDo * traiPhai, rb.velocity.y);
+
+        if(isFacingRight = true && traiPhai == -1)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if(isFacingRight = false && traiPhai == 1)
+        {
+            isFacingRight = true;
+        }
+        // Animation
+        
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private float remainingTime;
+    [SerializeField] private GameObject tryAgainPanel;
+    [SerializeField] private VideoPlayer tryAgainVideo; 
+
+    private bool isGameOver = false;
+    private bool isTimeRed = false;
+
+    void Start()
+    {
+        tryAgainPanel.SetActive(false);
+
+        if (tryAgainVideo != null)
+        {
+            tryAgainVideo.loopPointReached += OnVideoEnd; 
+            tryAgainVideo.Stop(); 
+        }
+    }
+
+    void Update()
+    {
+        if (isGameOver) return;
+
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+        }
+        else
+        {
+            remainingTime = 0;
+
+            if (!isTimeRed)
+            {
+                timerText.color = Color.red;
+                isTimeRed = true;
+                Invoke("GameOver", 1f);
+            }
+        }
+
+        int minutes = Mathf.FloorToInt(remainingTime / 60);
+        int seconds = Mathf.FloorToInt(remainingTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void GameOver()
+    {
+        isGameOver = true;
+        tryAgainPanel.SetActive(true); 
+
+        if (tryAgainVideo != null)
+        {
+            tryAgainVideo.Play(); 
+        }
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        Debug.Log("Video Try Again đã kết thúc!");
+    }
+}
