b53417f135580068e6a6a367dcd5495ff3f73ae9
Trịnh Kiên <trinhkien1211@gmail.com>
2025-03-19 16:28:33 +0700

    jump

--- Move.cs
+++ Move.cs
@@ -1,38 +1,49 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
     public Rigidbody2D rb;
-
     public Animator anim;
 
     public int tocDo = 4;
-
-    public float traiPhai;
-
+    public float jump = 7f; 
     public bool isFacingRight = true;
+    public bool isGrounded; 
 
-    void Start()
-    {
-        
-    }
+    public Transform groundCheck; 
+    public LayerMask groundLayer; 
 
     void Update()
     {
-        traiPhai = Input.GetAxisRaw("Horizontal"); // A = -1, 0, D = 1 
+        
+        float traiPhai = Input.GetAxisRaw("Horizontal"); // A = -1, D = 1, Không bấm = 0
         rb.velocity = new Vector2(tocDo * traiPhai, rb.velocity.y);
 
-        if(isFacingRight = true && traiPhai == -1)
+        
+        if (isFacingRight && traiPhai == -1)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            isFacingRight = false;
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
-        if(isFacingRight = false && traiPhai == 1)
+        else if (!isFacingRight && traiPhai == 1)
         {
             isFacingRight = true;
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
+
+        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+        }
+
         // Animation
-        anim.SetFloat("dichuyen", Mathf.Abs(traiPhai));
+       // anim.SetFloat("dichuyen", Mathf.Abs(traiPhai));
+      //  anim.SetBool("isJumping", !isGrounded);
     }
 }
